2241f20f99584d7e2a352034570756c00a1c2f77
tehnanali <49883700+tehnanali@users.noreply.github.com>
2020-05-12 05:50:55 +0500

    added class Main.java

---
+++ MemoryLeak/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		char[] c1 = new char[2000000000];
+		String s = new String(c1);
+		System.out.println(s.length());
+	}
+	}
+
